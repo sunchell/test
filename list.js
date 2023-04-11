ee9de91d9b2067e6c8235388c9d5a4afd63281a5
sunchell <130487965+sunchell@users.noreply.github.com>
2023-04-12 01:30:16 +0300

    list upd

--- list.js
+++ list.js
@@ -1,1 +1,1 @@
-var news = [{"content":"Test number 1. Qwerty"},{"content":"Test number 2. Uiopas"}];
+var news = [{"content":"Test number 1. Qwerty"},{"content":"Test number 2. Uiopas"},{"content":"Test number 3. Dfghjk"}];
